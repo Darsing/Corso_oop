9240bafa2a0ef74eab56c5a091c7792cb379184b
Darsing <vanessadjeumeni@yahoo.fr>
2023-03-06 06:58:31 +0100

    add

--- Corso_oop/Program.cs
+++ Corso_oop/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Corso_oop
 {
     //classe
     class Personne
     {
-        string nom;
+        
+        static int nombreDePersonnes = 0;
+
+        public string nom;
         int age;
         string emploi;
+        int numeroPersonne;
 
         //constructore
         public Personne  (string nom, int age, string emploi)
@@ -17,14 +22,25 @@ namespace Corso_oop
             this.age = age;
             this.emploi = emploi;
 
+            nombreDePersonnes++;
+            this.numeroPersonne = nombreDePersonnes;
+
         }
 
         //fonction
         public void Afficher()
         {
+            Console.WriteLine("Personne N°" + numeroPersonne);
             Console.WriteLine("Nom: " + nom);
             Console.WriteLine("Age: " + age + " ans");
             Console.WriteLine("Emploi: " + emploi);
+        }
+
+        public static void AfficherNombreDePersonnes()
+        {
+            //dans une fonction static ont peut acceder a la variable statique
+            Console.WriteLine("nombre total de personne : " + Personne.nombreDePersonnes);
+            
         }
     }
     internal class Program
@@ -36,18 +52,24 @@ namespace Corso_oop
             //personne1.Afficher();
 
             //creer une liste de personne
-            List<Personne> p = new List<Personne>()
+            List<Personne> personnes = new List<Personne>()
             {
                 new Personne("Paul",35, "Chauffeur"),
                 new Personne("Jacque",30, "Developpeur"),
                 new Personne("Anna",20, "Edutiante"),
+                new Personne("Juliette", 8, "cp"),
             };
 
-            foreach(Personne person in p)
+            //afficher par ordre alphabetique
+           // personnes = personnes.OrderBy(p => p.nom).ToList();
+
+            foreach(Personne person in personnes)
             {
                 person.Afficher();
             }
-            
+
+            Personne.AfficherNombreDePersonnes();
+           
 
 
         }
