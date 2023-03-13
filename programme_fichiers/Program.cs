70a8e475f207f3717d3c65bb1454ccbd51229471
Darsing <vanessadjeumeni@yahoo.fr>
2023-03-13 08:39:58 +0100

    aa

--- programme_fichiers/Program.cs
+++ programme_fichiers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace programme_fichiers
 {
@@ -6,7 +8,34 @@ namespace programme_fichiers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var noms = new List<string>()
+            {
+                "Jean",
+                "Paul",
+                "Maria",
+            };
+            //File.WriteAllText("monFichier.txt", "Voici le contenu que j ai dans mon fichier");
+            //File.AppendAllText("monfichier.txt", " je rajoute ce text.");
+            File.WriteAllLines("monfichier.txt",noms );
+            try
+            {
+                //string r = File.ReadAllText("monFichier.txt");
+                var ligne = File.ReadAllLines("monFichier.txt");
+                foreach(var line in ligne)
+                {
+                    Console.WriteLine(line);
+                }
+               
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("ERREUR : ce fichier n existe pas" + ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Une erreur inconnue est arrivee");
+            }
+             
         }
     }
 }
